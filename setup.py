3d15bd7e859cd3b18b4b52baf5707d4015d0dbbe
Viktor Shlapakov <vshlapakov@gmail.com>
2016-01-15 19:39:44 +0300

    An attempt to install sh_scrapy properly

--- setup.py
+++ setup.py
@@ -21,11 +21,7 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
     requires=['Scrapy (>=1.0.3)'],
-    install_requires=[
-        'scrapinghub-entrypoint-scrapy==0.4'
-    ],
     dependency_links=[
-        'http://github.com/scrapinghub/scrapinghub-entrypoint-scrapy/'
-        'tarball/0.4#egg=scrapinghub-entrypoint-scrapy',
+        'http://github.com/scrapinghub/scrapinghub-entrypoint-scrapy/tarball/0.4#egg=scrapinghub-entrypoint-scrapy',
     ],
 )
