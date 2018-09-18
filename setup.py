3d174d101f2080f16f5ab4e09bcde7a8fbde03f1
Ruairí Ó Fathaigh <ruairi.o.fathaigh@outlook.com>
2018-09-18 15:01:41 +0100

    Update supported python versions

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         'Scrapy>=1.0.3',
